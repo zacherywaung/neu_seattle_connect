c9461b43267bd7be41beb01e438455ce2c93f239
unknown <zhu.jiat@northeastern.edu>
2026-03-26 22:19:02 -0700

    feat: add signup/join for Events and Projects, course thread delete, notification support

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const NotificationSchema = new mongoose.Schema({
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   sender:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  type:      { type: String, enum: ['like', 'comment'], required: true },
+  type: { type: String, enum: ['like', 'comment', 'signup'], required: true },
   post:      { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
   read:      { type: Boolean, default: false },
 }, { timestamps: true });
